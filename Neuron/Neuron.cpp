72df9218648aeff686c289c4a513728aed950789
kn65op <kn65op@gmail.com>
2012-03-15 16:25:06 +0100

    Pierwszy commit

---
+++ Neuron/Neuron.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   Neuron.cpp
+ * Author: tomko
+ * 
+ * Created on 15 marzec 2012, 16:24
+ */
+
+#include "../headers/Neuron.h"
+
+Neuron::Neuron()
+{
+}
+
+Neuron::Neuron(const Neuron& orig)
+{
+}
+
+Neuron::~Neuron()
+{
+}
+
---
+++ Neuron/Neuron.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   Neuron.h
+ * Author: tomko
+ *
+ * Created on 15 marzec 2012, 16:24
+ */
+
+#ifndef NEURON_H
+#define	NEURON_H
+
+class Neuron
+{
+public:
+  Neuron();
+  Neuron(const Neuron& orig);
+  virtual ~Neuron();
+private:
+
+};
+
+#endif	/* NEURON_H */
+
