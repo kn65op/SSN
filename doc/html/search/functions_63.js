42e5325f68fe3bd4d5dbdcbef8dd0395e250899f
kn65op <kn65op@gmail.com>
2012-04-05 14:44:01 +0200

    Upgraded doc

--- doc/html/search/functions_63.js
+++ doc/html/search/functions_63.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['calcoutput',['calcOutput',['../class_neural_network.html#a7f03c156227f54548f92ea1cd686a7da',1,'NeuralNetwork']]],
+  ['calcoutput',['calcOutput',['../class_neural_network.html#af7c28ad226f6d32d8c578159f0d03ae9',1,'NeuralNetwork']]],
   ['calculateoutput',['calculateOutput',['../class_neuron.html#a36c60982cb604f2fa6a1765177a9c2e7',1,'Neuron']]]
 ];
