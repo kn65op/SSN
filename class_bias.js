e18ab4e87ff18bae94a25edb4276abb4286a8ed2
kn65op <kn65op@gmail.com>
2012-04-05 13:39:00 +0200

    Dox

---
+++ class_bias.js
@@ -0,0 +1,8 @@
+var class_bias =
+[
+    [ "Bias", "class_bias.html#abb349bc7ef3317db4a7ce75c96dbb533", null ],
+    [ "~Bias", "class_bias.html#a7e24089d25927e52514172e05f7e9177", null ],
+    [ "sendBiasToLinks", "class_bias.html#a3bf2cdb0a52508399df31b9f7dcf358b", null ],
+    [ "setBias", "class_bias.html#ab6df47ae8047f915cfa1fb77683b0eb7", null ],
+    [ "setBiasAndSend", "class_bias.html#a5489ed8d5ecb5bd14e21532968bb3027", null ]
+];
---
+++ search/functions_70.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['propagateanswer',['propagateAnswer',['../class_neuron.html#acb55ff03a770877b6afb86ed4d06896a',1,'Neuron']]]
+];
